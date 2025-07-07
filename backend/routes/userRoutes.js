d179103d814970f0f737506cf4322a940d46f233
KienDoTri <kiendotri@gmail.com>
2025-07-07 09:52:30 +0700

    xác thực bot

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js';
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses, verifyTurnstile } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
@@ -9,4 +9,5 @@ userRouter.post('/purchase', purchaseCourse);
 userRouter.post('/update-course-progress', updateUserCourseProgress);
 userRouter.post('/get-course-progress', getUserCourseProgress);
 userRouter.post('/add-rating', addUserRating);
+userRouter.post('/verify-turnstile', verifyTurnstile);
 export default userRouter;
